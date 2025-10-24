7d0d87f9052d028a5e31d961c6e637492b2ae5b9
joe <jonjy036@mymail.unisa.edu.au>
2025-10-24 11:33:17 +1030

    Download all assignment modules.
    Amend header docstring to include relevant details.

--- enclosure.py
+++ enclosure.py
@@ -1,8 +1,8 @@
 '''
-File: filename.py
-Description: A brief description of this Python module.
-Author: Billy Bizilis
-ID: 110100110
-Username: bizvy001
+File: enclosure.py
+Description: the enclosure module for the advanced OOP and Testing Assignment.
+Author: Jozef Jones
+ID: 110484756
+Username: jonjy036
 This is my own work as defined by the University's Academic Integrity Policy.
-''
+'''
