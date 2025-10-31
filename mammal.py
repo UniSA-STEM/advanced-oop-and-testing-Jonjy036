114eb7f2afb9f7bc72a73576ee2d061b029812d7
joe <jonjy036@mymail.unisa.edu.au>
2025-10-31 17:31:19 +1030

    Create Mammal class and define inherited methods.
    define new abstract method for specific to mammal class.
    
    Add docstrings for clarification of methods.

--- mammal.py
+++ mammal.py
@@ -6,3 +6,29 @@ ID: 110484756
 Username: jonjy036
 This is my own work as defined by the University's Academic Integrity Policy.
 '''
+from abc import abstractmethod
+from animal import Animal
+
+class Mammal(Animal):
+    def __init__(self, name, species, dob, gender, dietary_needs, is_mother):
+        super().__init__(name, species, dob, gender, dietary_needs, is_mother)
+
+    def eat(self):
+        '''Standard eating behaviour by all classes (except where necessary).'''
+        return f'{self.name} is eating. Munch munch.'
+
+    # Make_sound remains abstract as sound is bespoke to individual species.
+    @abstractmethod
+    def make_sound(self):
+        '''Animal Specific sound. this must be implemented by a child class'''
+        pass
+
+    def sleep(self):
+        '''Standard sleeping behaviour by all classes (except where necessary).'''
+        return f'Shhhhh. {self.name} is sleeping.'
+
+    # New abstract method specific to mammals.
+    @abstractmethod
+    def nurse_young(self):
+        '''Abstract nursing behaviour which is bespoke to mammals.'''
+        pass
