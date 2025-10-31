108de7c6e796f5db98f06ff959907b95017ec869
joe <jonjy036@mymail.unisa.edu.au>
2025-10-31 15:22:36 +1030

    Create module for 'bird' which will be a child of 'animal'.
    Add university header.

---
+++ bird.py
@@ -0,0 +1,8 @@
+'''
+File: mammal.py
+Description: the bird child module (animal) for the advanced OOP and Testing Assignment.
+Author: Jozef Jones
+ID: 110484756
+Username: jonjy036
+This is my own work as defined by the University's Academic Integrity Policy.
+'''
