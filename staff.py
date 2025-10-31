75d65639778e0eb9cf57f8e47547928549683b71
joe <jonjy036@mymail.unisa.edu.au>
2025-10-31 14:07:36 +1030

    Add property decorators for getters and setters.
    Adjust parameter to take dob rather than age.
    
    Property for age to utilise the dob, therefore no setter required.

--- staff.py
+++ staff.py
@@ -7,10 +7,11 @@ Username: jonjy036
 This is my own work as defined by the University's Academic Integrity Policy.
 '''
 from abc import ABC, abstractmethod
+from datetime import date
 
 # Define Grandparent class of Animal.
 class Animal(ABC):
-    def __init__(self, name: str, species: str, age: int, dietary_needs: str):
+    def __init__(self, name: str, species: str, dob: date, dietary_needs: str):
 
         # Validate parameters.
         if not isinstance(name, str):
@@ -23,10 +24,10 @@ class Animal(ABC):
         elif not species:
             raise ValueError('Species cannot be empty')
 
-        if not isinstance(age, int):
-            raise TypeError('Age must be an integer')
-        elif not age:
-            raise ValueError('Age cannot be empty')
+        if not isinstance(dob, date):
+            raise TypeError('Age must be a date')
+        elif not dob:
+            raise ValueError('DOB cannot be empty')
 
         if not isinstance(dietary_needs, str):
             raise TypeError('Dietary needs must be a string')
@@ -35,7 +36,36 @@ class Animal(ABC):
 
         self.__name = name
         self.__species = species
-        self.__age = age
+        self.__dob = dob
+        self.__dietary_needs = dietary_needs
+
+    # Add property decorators for getters and setters.
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def species(self):
+        return self.__species
+
+    @property
+    def age(self):
+        today = date.today()
+        years = today.year - self.__dob.year
+        if (today.month, today.day) < (self.__dob.month, self.__dob.day):
+            years -= 1
+        return f'{years} years old'
+
+    @property
+    def dietary_needs(self):
+        return self.__dietary_needs
+
+    @dietary_needs.setter
+    def dietary_needs(self, dietary_needs):
+        if not isinstance(dietary_needs, str):
+            raise TypeError('Dietary needs must be a string')
+        elif not dietary_needs:
+            raise ValueError('Dietary needs cannot be empty')
         self.__dietary_needs = dietary_needs
 
     # Define abstract methods to pass to children and grandchildren classes.
