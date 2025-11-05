721e54af813b8c8610c2e3509dd14210092bf309
joe <jonjy036@mymail.unisa.edu.au>
2025-11-05 11:10:42 +1030

    Create new module to hold species dictionaries.
    Define key species information in dictionaries.

---
+++ species_data.py
@@ -0,0 +1,35 @@
+SPECIES_ENVIRONMENT = {
+    'lion': 'savannah',
+    'chimpanzee': 'forest',
+    'dingo': 'outback',
+    'crocodile': 'aquatic',
+    'python': 'forest vivarium',
+    'bearded dragon': 'desert vivarium',
+    'eagle': 'savannah',
+    'kookaburra': 'forest',
+    'penguin': 'aquatic'
+}
+
+ENVIRONMENT_SIZE = {
+    'lion': ['extra large'],
+    'chimpanzee': ['large', 'extra large'],
+    'dingo': ['large', 'extra large'],
+    'crocodile': ['large', 'extra large'],
+    'python': ['medium'],
+    'bearded dragon': ['small'],
+    'eagle': ['large'],
+    'kookaburra': ['medium'],
+    'penguin': ['medium', 'large'],
+}
+
+SPECIES_DIETARY_NEEDS = {
+    'lion': ['meat'],
+    'chimpanzee': ['fruit', 'vegetable'],
+    'dingo': ['meat'],
+    'crocodile': ['meat'],
+    'python': ['rodents', 'meat'],
+    'bearded dragon': ['insects', 'green leaves'],
+    'eagle': ['rodents', 'meat'],
+    'kookaburra': ['insects', 'rodents'],
+    'penguin': ['fish']
+}
