6ab89daf4c98bcd3e621c3629e94c1b6debf4bec
joe <jonjy036@mymail.unisa.edu.au>
2025-11-05 15:34:02 +1030

    Define a basic Staff parent class.

--- staff.py
+++ staff.py
@@ -6,4 +6,25 @@ ID: 110484756
 Username: jonjy036
 This is my own work as defined by the University's Academic Integrity Policy.
 '''
+class Staff:
+    def __init__(self, name: str, staff_number: int):
+        self.__name = name
+        self.__staff_number = staff_number
+
+    def __str__(self):
+        return (f'Name: {self.__name}\n'
+                f'Staff number: {self.__staff_number}\n')
+
+    @property
+    def name(self)
+        return self.__name
+
+    @property
+    def staff_number(self)
+        return self.__staff_number
+
+    @name.setter
+    def name(self, name):
+        self.__name = name
+
 
