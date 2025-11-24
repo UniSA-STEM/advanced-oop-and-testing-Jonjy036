b65696a2bb834315815feec33f45aa0f05bb1dba
joe <jonjy036@mymail.unisa.edu.au>
2025-11-24 18:54:55 +1030

    edit docstring identifier.

--- test_staff.py
+++ test_staff.py
@@ -1,11 +1,11 @@
-'''
+"""
 File: test_staff.py
 Description: the staff testing module for the advanced OOP and Testing Assignment.
 Author: Jozef Jones
 ID: 110484756
 Username: jonjy036
 This is my own work as defined by the University's Academic Integrity Policy.
-'''
+"""
 import pytest
 from datetime import date
 
